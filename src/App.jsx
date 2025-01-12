ef9b1c5d189e0162487703adae7b6d25da9733bd
Ansh Prajapat <anshprajapat8@gmail.com>
2025-01-12 18:50:14 +0530

    change in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,15 @@ function App() {
   return (
     <div className='h-screen w-full flex justify-center items-center flex-col gap-3'>
 
-      <Link to={'/Webito-Task/form'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>Form validation</Link>
+      <Link to={'form'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>Form validation</Link>
 
-      <Link to={'/Webito-Task/effect-ref'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>useEffect, useRef</Link>
+      <Link to={'effect-ref'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>useEffect, useRef</Link>
 
-      <Link to={'/Webito-Task/callback'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>useCallback</Link>
+      <Link to={'callback'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>useCallback</Link>
 
-      <Link to={'/Webito-Task/memo'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>useMemo</Link>
+      <Link to={'memo'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>useMemo</Link>
 
-      <Link to={'/Webito-Task/context'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>useContext</Link>
+      <Link to={'context'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>useContext</Link>
 
     </div>
   )
