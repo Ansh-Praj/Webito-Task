ce37d776050431b9603b94cfbddcd18c185e6460
Ansh Prajapat <anshprajapat8@gmail.com>
2025-01-12 20:16:22 +0530

    addes basename

--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,29 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './Index.css';
-import { createBrowserRouter, createRoutesFromChildren, Route, Routes, RouterProvider } from 'react-router';
+import { createBrowserRouter, createRoutesFromChildren, Route, Routes, RouterProvider, createRoutesFromElements } from 'react-router';
 import Form from './components/Form.jsx';
 import Callback from './components/Callback.jsx';
 import Context from './components/Context.jsx';
 import Effect from './components/Effect.jsx';
 import Memo from './components/Memo.jsx';
 
-// Use createRoutesFromChildren with the Routes wrapper
-const router = createBrowserRouter(
-  createRoutesFromChildren(
+const routes = createRoutesFromElements(
     <>
-      <Route path="Webito-Task/" element={<App />} />
-      <Route path="/Webito-Task/form" element={<Form />} />
-      <Route path="/Webito-Task/effect-ref" element={<Effect />} />
-      <Route path="/Webito-Task/callback" element={<Callback />} />
-      <Route path="/Webito-Task/memo" element={<Memo />} />
-      <Route path="/Webito-Task/context" element={<Context />} />
+      <Route path="/" element={<App />} />
+
+      <Route path="/form" element={<Form />} />
+      <Route path="/effect-ref" element={<Effect />} />
+      <Route path="/callback" element={<Callback />} />
+      <Route path="/memo" element={<Memo />} />
+      <Route path="/context" element={<Context />} />
     </>
-  )
+  
 );
+
+const router = createBrowserRouter(routes, {
+    basename: "/Webito-Task"
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
