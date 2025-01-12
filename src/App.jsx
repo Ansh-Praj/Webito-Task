554b2beefb2508499c0790896787f2bf744e3fec
Ansh Prajapat <anshprajapat8@gmail.com>
2025-01-12 17:52:23 +0530

    Changes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ function App() {
 
       <Link to={'Webito-Task/form'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>Form validation</Link>
 
-      <Link to={'/effecWebito-Taskt-ref'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>useEffect, useRef</Link>
+      <Link to={'Webito-Task/effect-ref'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>useEffect, useRef</Link>
 
-      <Link to={'/calWebito-Tasklback'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>useCallback</Link>
+      <Link to={'Webito-Task/callback'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>useCallback</Link>
 
       <Link to={'Webito-Task/memo'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>useMemo</Link>
 
-      <Link to={'/coWebito-Taskntext'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>useContext</Link>
+      <Link to={'Webito-Task/context'} className='load w-1/3 py-3 rounded-lg text-center bg-white text-slate-800 font-semibold text-2xl hover:bg-black hover:text-white hover:border-white border-2 transition ease-in-out duration-300'>useContext</Link>
 
     </div>
   )
